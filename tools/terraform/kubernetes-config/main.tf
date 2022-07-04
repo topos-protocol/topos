4b8446c7af2a0a10500ff318843fd5816c7575fb
sebastiendan <sebastien.dan@gmail.com>
2022-07-04 17:28:35 -0400

    ci: add docker and k8s workflows for e2e tests

---
+++ tools/terraform/kubernetes-config/main.tf
@@ -0,0 +1,273 @@
+terraform {
+  required_providers {
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+      version = ">= 2.11.0"
+    }
+  }
+}
+
+resource "kubernetes_namespace" "namespace" {
+  metadata {
+    name = "test"
+  }
+}
+
+resource "kubernetes_secret" "secret" {
+  metadata {
+    name = "docker-cfg"
+    namespace = kubernetes_namespace.namespace.metadata.0.name
+  }
+
+  data = {
+    ".dockerconfigjson" = jsonencode({
+      auths = {
+        "${var.docker_registry}" = {
+          "username" = var.docker_registry_auth_user
+          "password" = var.docker_registry_auth_pwd
+          "auth"     = base64encode("${var.docker_registry_auth_user}:${var.docker_registry_auth_pwd}")
+        }
+      }
+    })
+  }
+
+  type = "kubernetes.io/dockerconfigjson"
+}
+
+resource "kubernetes_deployment" "bootnode" {
+  metadata {
+    name = "bootnode"
+    namespace= kubernetes_namespace.namespace.metadata.0.name
+  }
+  spec {
+    replicas = 1
+
+    selector {
+      match_labels = {
+        app = "TCE-Bootnode"
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          app  = "TCE-Bootnode"
+        }
+      }
+      spec {
+        image_pull_secrets {
+          name = kubernetes_secret.secret.metadata.0.name
+        }
+
+        container {
+          image = var.tce_node_image
+          name  = "tce"
+
+          port {
+            name = "tcp"
+            container_port = "9090"
+          }
+
+          port {
+            name = "http"
+            container_port = "8080"
+            protocol = "TCP"
+          }
+
+          env {
+            name  = "TCE_LOCAL_KS"
+            value = 1 # 12D3KooWPjceQrSwdWXPyLLeABRXmuqt69Rg3sBYbU1Nft9HyQ6X
+          }
+
+          env {
+            name  = "TCE_JAEGER_AGENT"
+            value = var.jaeger_endpoint
+          }
+        }
+      }
+    }
+  }
+}
+
+# resource "kubernetes_pod" "bootnode" {
+#   metadata {
+#     name = "tce-boot"
+#     namespace= kubernetes_namespace.namespace.metadata.0.name
+#     labels = {
+#       app = "TCE-Bootnode"
+#     }
+#   }
+#   spec {
+#     image_pull_secrets {
+#       name = kubernetes_secret.secret.metadata.0.name
+#     }
+
+#     container {
+#       image = var.tce_node_image
+#       name  = "tce"
+
+#       port {
+#         name = "tcp"
+#         container_port = "9090"
+#         protocol = "TCP"
+#       }
+
+#       port {
+#         name = "http"
+#         container_port = "8080"
+#         protocol = "TCP"
+#       }
+      
+#       env {
+#         name  = "TCE_LOCAL_KS"
+#         value = 1 # 12D3KooWPjceQrSwdWXPyLLeABRXmuqt69Rg3sBYbU1Nft9HyQ6X
+#       }
+
+#       env {
+#         name  = "TCE_JAEGER_AGENT"
+#         value = var.jaeger_endpoint
+#       }
+#     }
+#   }
+# }
+
+resource "kubernetes_service" "bootnode" {
+  metadata {
+    name = "tce-boot"
+    namespace = "${kubernetes_namespace.namespace.metadata.0.name}"
+  }
+  spec {
+    selector = {
+      app = kubernetes_deployment.bootnode.spec.0.template.0.metadata.0.labels.app
+    }
+
+    session_affinity = "ClientIP"
+
+    port {
+      port        = 9090
+      target_port = "tcp"
+    }
+  }
+}
+
+resource "kubernetes_deployment" "replicas" {
+  metadata {
+    name = "tce-replicas"
+    namespace= kubernetes_namespace.namespace.metadata.0.name
+  }
+  spec {
+    replicas = var.replica_count
+
+    selector {
+      match_labels = {
+        app = "TCE-Replicas"
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          app  = "TCE-Replicas"
+        }
+      }
+      spec {
+
+        image_pull_secrets {
+          name = kubernetes_secret.secret.metadata.0.name
+        }
+
+        container {
+          image = var.tce_node_image
+          name  = "tce"
+
+          port {
+            name = "tcp"
+            container_port = "9090"
+          }
+
+          port {
+            name = "http"
+            container_port = "8080"
+            protocol = "TCP"
+          }
+
+          env {
+            name  = "TCE_JAEGER_AGENT"
+            value = var.jaeger_endpoint
+          }
+
+          env {
+            name  = "TCE_BOOT_PEERS"
+            value = "12D3KooWPjceQrSwdWXPyLLeABRXmuqt69Rg3sBYbU1Nft9HyQ6X /dns4/${kubernetes_service.bootnode.metadata.0.name}/tcp/${kubernetes_service.bootnode.spec.0.port.0.port}"
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "replicas" {
+  metadata {
+    name = "replicas"
+    namespace = "${kubernetes_namespace.namespace.metadata.0.name}"
+  }
+  spec {
+    selector = {
+      app = kubernetes_deployment.replicas.spec.0.template.0.metadata.0.labels.app
+    }
+
+    session_affinity = "ClientIP"
+
+    port {
+      port        = 8080
+      target_port = "http"
+    }
+  }
+}
+
+resource "kubernetes_deployment" "cert_spammer" {
+  metadata {
+    name = "cert-spammer"
+    namespace= kubernetes_namespace.namespace.metadata.0.name
+  }
+  spec {
+    replicas = 1
+
+    selector {
+      match_labels = {
+        app = "Cert-Spammer"
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          app  = "Cert-Spammer"
+        }
+      }
+      spec {
+
+        image_pull_secrets {
+          name = kubernetes_secret.secret.metadata.0.name
+        }
+
+        container {
+          image = var.cert_spammer_image
+          name  = "cert-spammer"
+
+          env {
+            name  = "TARGET_NODE"
+            value = "${kubernetes_service.replicas.metadata.0.name}:${kubernetes_service.replicas.spec.0.port.0.port}"
+          }
+          
+          env {
+            name  = "RUST_LOG"
+            value = "info"
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "local_file" "kubeconfig" {
+  content = var.kubeconfig
+  filename = "${path.root}/kubeconfig"
+}
