3bb6e8c35c92e4d19812559fa45eeae43ae3b532
sebastiendan <sebastien.dan@gmail.com>
2022-08-24 14:42:04 -0400

    ci: dummy trigger

--- node/store/src/lib.rs
+++ node/store/src/lib.rs
@@ -4,6 +4,7 @@ use tce_trbp::{trb_store::TrbStore, Errors};
 use topos_core::uci::{Certificate, CertificateId, DigestCompressed, SubnetId};
 
 /// Configuration of RocksDB store
+/// Configuration of RocksDB store
 pub struct StoreConfig {
     pub db_path: String,
 }
