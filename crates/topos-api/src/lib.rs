7bc6c7496c0c4f76007716a592cab53a9286e3bf
Freyskeyd <simon.paitrault@gmail.com>
2022-08-29 14:08:12 +0200

    feat: expose protobuf client/server in lib
    Signed-off-by: Freyskeyd <simon.paitrault@gmail.com>

--- crates/topos-api/src/lib.rs
+++ crates/topos-api/src/lib.rs
@@ -1,8 +1,5 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
+#[path = "generated"]
+pub mod tce {
+    #[path = "topos.tce.v1.rs"]
+    pub mod v1;
 }
