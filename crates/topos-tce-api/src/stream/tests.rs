5fc1501ce2ee1cedfaa5eda35707099f83b60d93
Freyskeyd <simon.paitrault@gmail.com>
2022-09-08 16:30:54 +0200

    chore: update code to match topos-core update
    Signed-off-by: Freyskeyd <simon.paitrault@gmail.com>

--- crates/topos-tce-api/src/stream/tests.rs
+++ crates/topos-tce-api/src/stream/tests.rs
@@ -3,39 +3,12 @@ use crate::tests::encode;
 use crate::wait_for_command;
 use test_log::test;
 use tokio::spawn;
-use topos_core::api::tce::v1::watch_certificates_request::{OpenStream, PauseStream};
+use topos_core::api::tce::v1::watch_certificates_request::OpenStream;
 use topos_core::api::tce::v1::WatchCertificatesRequest;
 
 use self::utils::StreamBuilder;
 
 mod utils;
-
-#[test(tokio::test)]
-pub async fn sending_pause_stream_before_open() -> Result<(), Box<dyn std::error::Error>> {
-    let (mut tx, stream, mut context) = StreamBuilder::default().build();
-
-    let msg: WatchCertificatesRequest = PauseStream {}.into();
-    {
-        _ = tx.send_data(encode(&msg)?).await;
-        let _ = tx;
-    }
-
-    spawn(stream.run());
-
-    wait_for_command!(
-        context.stream_receiver,
-        matches: Err(status) if status.message() == "No openstream provided"
-    );
-
-    let expected_stream_id = context.stream_id;
-
-    wait_for_command!(
-        context.runtime_receiver,
-        matches: InternalRuntimeCommand::StreamTimeout { stream_id } if stream_id == expected_stream_id
-    );
-
-    Ok(())
-}
 
 #[test(tokio::test)]
 pub async fn sending_no_message() -> Result<(), Box<dyn std::error::Error>> {
@@ -65,7 +38,7 @@ pub async fn sending_open_stream_message() -> Result<(), Box<dyn std::error::Err
     spawn(async move { stream.run().await });
 
     let msg: WatchCertificatesRequest = OpenStream {
-        subnet_id: "subnet_id".to_string(),
+        subnet_ids: vec!["subnet_id".into()],
     }
     .into();
     {
@@ -77,7 +50,7 @@ pub async fn sending_open_stream_message() -> Result<(), Box<dyn std::error::Err
 
     wait_for_command!(
         context.runtime_receiver,
-        matches: InternalRuntimeCommand::Register { stream_id, ref subnet_id, .. } if stream_id == expected_stream_id && subnet_id == "subnet_id"
+        matches: InternalRuntimeCommand::Register { stream_id, ref subnet_ids, .. } if stream_id == expected_stream_id && subnet_ids == &vec!["subnet_id".into()]
     );
 
     Ok(())
