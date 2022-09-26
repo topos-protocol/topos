c5226c2b6e43247ab3e11433955c58ddffb924d0
Freyskeyd <simon.paitrault@gmail.com>
2022-09-26 11:53:08 +0200

    chore: let the code breath
    Signed-off-by: Freyskeyd <simon.paitrault@gmail.com>

--- src/cli.rs
+++ src/cli.rs
@@ -10,34 +10,44 @@ pub struct AppArgs {
     /// quoted list like --boot-peers='a a1 b b1'
     #[clap(long, default_value = "", env = "TCE_BOOT_PEERS")]
     pub boot_peers: String,
+
     /// Advertised (externally visible) <host|address:port>,
     /// if empty this machine ip address(es) are used
     #[clap(long, env = "TCE_EXT_HOST")]
     pub tce_ext_host: Option<String>,
+
     /// Port to listen on (host is 0.0.0.0, should be good for most installations)
     #[clap(long, default_value_t = 0, env = "TCE_PORT")]
     pub tce_local_port: u16,
+
     /// WebAPI external url <host|address:port> (optional)
     #[clap(long, env = "TCE_WEB_API_EXT_URL")]
     pub web_api_ext_url: Option<String>,
+
     /// WebAPI port
     #[clap(long, default_value_t = 8080, env = "TCE_WEB_API_PORT")]
     pub web_api_local_port: u16,
+
     /// Local peer secret key seed (optional, used for testing)
     #[clap(long, env = "TCE_LOCAL_KS")]
     pub local_key_seed: Option<u8>,
+
     /// Local peer key-pair (in base64 format)
     #[clap(long, env = "TCE_LOCAL_KEYPAIR")]
     pub local_key_pair: Option<String>,
+
     /// Storage database path, if not set RAM storage is used
     #[clap(long, env = "TCE_DB_PATH")]
     pub db_path: Option<String>,
+
     /// Socket of the Jaeger agent endpoint
     #[clap(long, default_value = "127.0.0.1:6831", env = "TCE_JAEGER_AGENT")]
     pub jaeger_agent: String,
+
     /// Testing only - deliver certificate immediately upon submission
     #[clap(long, env = "TCE_TEST_IMMEDIATE_DELIVERY")]
     pub test_immediate_delivery: bool,
+
     /// TRBP parameters
     #[clap(flatten)]
     pub trbp_params: ReliableBroadcastParams,
