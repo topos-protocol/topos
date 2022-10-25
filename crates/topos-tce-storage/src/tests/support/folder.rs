1d8c127b3b9989704483a0accc3ecf89c6910b78
Simon Paitrault <simon.paitrault@gmail.com>
2022-10-25 11:05:48 +0200

    feat: add tests to tce-storage (#54)

---
+++ crates/topos-tce-storage/src/tests/support/folder.rs
@@ -0,0 +1,22 @@
+use std::{
+    fs,
+    path::{Path, PathBuf},
+};
+
+use rand::Rng;
+use rstest::fixture;
+
+#[fixture]
+pub(crate) fn random_path() -> Box<PathBuf> {
+    let mut rng = rand::thread_rng();
+
+    let random: u32 = rng.gen();
+    Box::new(Path::new(&format!("./tests/databases/{random}")).to_path_buf())
+}
+
+#[fixture]
+pub(crate) fn created_folder(random_path: Box<PathBuf>) -> Box<PathBuf> {
+    fs::create_dir_all(*random_path.clone()).unwrap();
+
+    random_path
+}
