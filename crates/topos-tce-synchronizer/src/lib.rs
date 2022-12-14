5e82bfeb0b8cdfe116894d30dad6fda872819596
Freyskeyd <simon.paitrault@gmail.com>
2022-12-14 12:28:55 +0100

    feat: add CheckpointsCollector to synchronizer
    Signed-off-by: Freyskeyd <simon.paitrault@gmail.com>

--- crates/topos-tce-synchronizer/src/lib.rs
+++ crates/topos-tce-synchronizer/src/lib.rs
@@ -1,20 +1,30 @@
 use std::future::IntoFuture;
 
 use builder::SynchronizerBuilder;
+use checkpoints_collector::{
+    CheckpointsCollectorClient, CheckpointsCollectorError, CheckpointsCollectorEvent,
+};
 use futures::{future::BoxFuture, FutureExt};
 use thiserror::Error;
 use tokio::sync::mpsc;
+use tokio_stream::StreamExt;
 
 mod builder;
+mod checkpoints_collector;
 mod client;
 
 pub use client::SynchronizerClient;
+use tokio_stream::wrappers::ReceiverStream;
 
 pub struct Synchronizer {
     pub(crate) shutdown: mpsc::Receiver<()>,
     pub(crate) commands: mpsc::Receiver<SynchronizerCommand>,
     #[allow(dead_code)]
     pub(crate) events: mpsc::Sender<SynchronizerEvent>,
+
+    #[allow(dead_code)]
+    pub(crate) checkpoints_collector: CheckpointsCollectorClient,
+    pub(crate) checkpoints_collector_stream: ReceiverStream<CheckpointsCollectorEvent>,
 }
 
 impl IntoFuture for Synchronizer {
@@ -28,6 +38,7 @@ impl IntoFuture for Synchronizer {
                 tokio::select! {
                     _ = self.shutdown.recv() => { break; }
                     _command = self.commands.recv() => {}
+                    _checkpoint_event = self.checkpoints_collector_stream.next() => {}
                 }
             }
 
@@ -45,6 +56,9 @@ impl Synchronizer {
 }
 
 #[derive(Error, Debug)]
-pub enum SynchronizerError {}
+pub enum SynchronizerError {
+    #[error("Error while dealing with CheckpointsCollector: {0}")]
+    CheckpointsCollectorError(#[from] CheckpointsCollectorError),
+}
 pub enum SynchronizerCommand {}
 pub enum SynchronizerEvent {}
