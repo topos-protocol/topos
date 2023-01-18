c9a886b947774f2f378e6727a7feafe338832fdc
Thiago Nobayashi <thiago.nobayashi@toposware.com>
2023-01-18 11:06:11 -0300

    feat: create and destroy cluster

---
+++ tools/terraform/cicd-devnet-1/main.tf
@@ -0,0 +1,42 @@
+variable "region" {}
+variable "cidr_block" {}
+variable "min_num_nodes" {}
+variable "max_num_nodes" {}
+
+# Recipe to create a k8s cluster in 2 AZs
+# access to the internet provided by 2 public subnets
+
+locals {
+  subnets = cidrsubnets("${var.cidr_block}", "2", "2", "2", "2")
+}
+
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+provider "aws" {
+  region = var.region
+}
+
+module "eks" {
+  source = "git::https://github.com/toposware/infrastructure-as-code.git//terraform/modules/aws/eks"
+
+  name   = "topos-devnet-1-observability"
+  region = var.region
+
+  cidr_block         = var.cidr_block
+  private_subnets    = slice(local.subnets, 0, 2)
+  public_subnets     = slice(local.subnets, 2, 4)
+  availability_zones = data.aws_availability_zones.available
+
+  min_num_nodes = var.min_num_nodes
+  max_num_nodes = var.max_num_nodes
+}
+
+output "cluster_name" {
+  value = module.eks.cluster_name
+}
+
+output "eks_irsa_arn" {
+  value = module.eks.eks_irsa_arn
+}
---
+++ tools/terraform/cicd-devnet-1/terraform.tfvars
@@ -0,0 +1,4 @@
+region     = "us-east-1"
+cidr_block = "10.16.0.0/16"
+max_num_nodes = 2
+min_num_nodes = 2
