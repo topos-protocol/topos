32ab337cf65aa3ecda2c0f5a9715714851b37605
Thiago Nobayashi <thiago.nobayashi@toposware.com>
2023-01-19 20:54:33 -0300

    chore: tmp changes for remote module

--- tools/terraform/cicd-devnet-1/main.tf
+++ tools/terraform/cicd-devnet-1/main.tf
@@ -19,7 +19,7 @@ provider "aws" {
 }
 
 module "eks" {
-  source = "git::https://github.com/toposware/infrastructure-as-code.git//terraform/modules/aws/eks"
+  source = "git::https://github.com/toposware/infrastructure-as-code.git//terraform/modules/aws/eks?ref=feat/add-efs"
 
   name   = "cicd-devnet-1"
   region = var.region
