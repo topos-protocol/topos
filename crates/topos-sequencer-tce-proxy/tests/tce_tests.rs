d6ddeddfbf17c2e0cf09c7fc37b98b22a8e9290b
Marko Atanasievski <atanmarko@users.noreply.github.com>
2023-02-10 12:12:45 +0100

    feat: remove tce mock node (#134)
    feat: remove tce mock and use actual tce in sequencer tests

--- crates/topos-sequencer-tce-proxy/tests/tce_tests.rs
+++ crates/topos-sequencer-tce-proxy/tests/tce_tests.rs
@@ -2,7 +2,11 @@ use futures::{Future, StreamExt};
 use rstest::*;
 use serial_test::serial;
 use test_log::test;
-use topos_core::api::shared::v1::{CertificateId, SubnetId};
+use tokio::{
+    sync::{mpsc, oneshot},
+    time::Duration,
+};
+use topos_core::api::shared::v1::{CertificateId, StarkProof, SubnetId};
 use topos_core::api::tce::v1::{
     watch_certificates_request, watch_certificates_response,
     watch_certificates_response::CertificatePushed, GetSourceHeadRequest, GetSourceHeadResponse,
@@ -13,34 +17,41 @@ use tracing::{debug, error, info};
 
 mod common;
 
+const TCE_NODE_STARTUP_DELAY: Duration = Duration::from_secs(5);
+
 #[allow(dead_code)]
 struct Context {
     endpoint: String,
-    service_handle: tokio::task::JoinHandle<()>,
-    shutdown_tce_node_signal: futures::channel::oneshot::Sender<()>,
+    shutdown_tce_node_signal: mpsc::Sender<oneshot::Sender<()>>,
 }
 
 impl Context {
     pub async fn shutdown(self) -> Result<(), Box<dyn std::error::Error>> {
-        self.shutdown_tce_node_signal.send(()).unwrap();
+        info!("Context performing shutdown...");
+        let (shutdown_finished_sender, shutdown_finished_receiver) = oneshot::channel::<()>();
+        self.shutdown_tce_node_signal
+            .send(shutdown_finished_sender)
+            .await
+            .unwrap();
+
+        shutdown_finished_receiver.await.unwrap();
+        info!("Shutdown finished...");
+
         Ok(())
     }
 }
 
 #[fixture]
-async fn context_running_tce_mock_node() -> Context {
-    info!("Starting TCE node mock service...");
-    let (handle, shutdown_tce_node_signal, endpoint) = match common::start_mock_tce_service().await
-    {
+async fn context_running_tce_test_node() -> Context {
+    let (shutdown_tce_node_signal, endpoint) = match common::start_tce_test_service().await {
         Ok(result) => result,
         Err(e) => {
             panic!("Unable to start mock tce node, details: {e}");
         }
     };
-    tokio::time::sleep(tokio::time::Duration::from_millis(1000)).await;
+    tokio::time::sleep(TCE_NODE_STARTUP_DELAY).await;
     Context {
         endpoint,
-        service_handle: handle,
         shutdown_tce_node_signal,
     }
 }
@@ -48,10 +59,36 @@ async fn context_running_tce_mock_node() -> Context {
 #[rstest]
 #[test(tokio::test)]
 #[serial]
-async fn test_tce_submit_certificate(
-    context_running_tce_mock_node: impl Future<Output = Context>,
+async fn test_run_tce_node(
+    context_running_tce_test_node: impl Future<Output = Context>,
 ) -> Result<(), Box<dyn std::error::Error>> {
-    let context = context_running_tce_mock_node.await;
+    let context = context_running_tce_test_node.await;
+
+    info!("Creating TCE node client");
+    let _client = match topos_core::api::tce::v1::api_service_client::ApiServiceClient::connect(
+        format!("http://{}", context.endpoint),
+    )
+    .await
+    {
+        Ok(client) => client,
+        Err(e) => {
+            error!("Unable to create client, error: {}", e);
+            return Err(Box::from(e));
+        }
+    };
+
+    info!("Shutting down TCE node client");
+    context.shutdown().await?;
+    Ok(())
+}
+
+#[rstest]
+#[test(tokio::test)]
+#[serial]
+async fn test_tce_submit_certificate(
+    context_running_tce_test_node: impl Future<Output = Context>,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let context = context_running_tce_test_node.await;
 
     let source_subnet_id: SubnetId = [1u8; 32].into();
     let prev_certificate_id: CertificateId = [01u8; 32].into();
@@ -77,7 +114,11 @@ async fn test_tce_submit_certificate(
                 id: Some(certificate_id),
                 prev_id: Some(prev_certificate_id),
                 target_subnets: vec![],
-                ..Default::default()
+                state_root: [0u8; 32].to_vec(),
+                tx_root_hash: [0u8; 32].to_vec(),
+                verifier: 0,
+                proof: Some(StarkProof { value: Vec::new() }),
+                signature: Some(Default::default()),
             }),
         })
         .await
@@ -100,20 +141,12 @@ async fn test_tce_submit_certificate(
 #[test(tokio::test)]
 #[serial]
 async fn test_tce_watch_certificates(
-    context_running_tce_mock_node: impl Future<Output = Context>,
+    context_running_tce_test_node: impl Future<Output = Context>,
 ) -> Result<(), Box<dyn std::error::Error>> {
-    let context = context_running_tce_mock_node.await;
+    let context = context_running_tce_test_node.await;
 
     let source_subnet_id: SubnetId = SubnetId {
         value: [1u8; 32].to_vec(),
-    };
-
-    let expected_certificate = CertificateId {
-        value: [
-            1u8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-            0, 0, 0, 0,
-        ]
-        .to_vec(),
     };
 
     info!("Creating TCE node client");
@@ -136,34 +169,31 @@ async fn test_tce_watch_certificates(
     };
     let response = client.watch_certificates(in_stream).await.unwrap();
     let mut resp_stream = response.into_inner();
-    debug!("TCE client: waiting for watch certificate response");
+    info!("TCE client: waiting for watch certificate response");
     while let Some(received) = resp_stream.next().await {
-        debug!("TCE client received: {:?}", received);
+        info!("TCE client received: {:?}", received);
         let received = received.unwrap();
         match received.event {
             Some(watch_certificates_response::Event::CertificatePushed(CertificatePushed {
                 certificate: Some(certificate),
             })) => {
-                debug!("Certificate received {:?}", certificate);
-                assert_eq!(certificate.id, Some(expected_certificate));
-                assert_eq!(certificate.source_subnet_id, Some(source_subnet_id));
-                break;
+                info!("Certificate received {:?}", certificate);
             }
             Some(watch_certificates_response::Event::StreamOpened(
                 watch_certificates_response::StreamOpened { subnet_ids },
             )) => {
                 debug!("TCE client: stream opened for subnet_ids {:?}", subnet_ids);
                 assert_eq!(subnet_ids[0].value, source_subnet_id.value);
+                // We have opened connection and 2 way stream, finishing test
+                break;
             }
             Some(watch_certificates_response::Event::CertificatePushed(CertificatePushed {
                 certificate: None,
             })) => {
-                error!("TCE client: empty certificate received");
                 panic!("TCE client: empty certificate received");
             }
             _ => {
-                error!("TCE client: something unexpected is received");
-                panic!("None received");
+                panic!("TCE client: something unexpected is received");
             }
         }
     }
@@ -176,12 +206,12 @@ async fn test_tce_watch_certificates(
 #[test(tokio::test)]
 #[serial]
 async fn test_tce_get_source_head_certificate(
-    context_running_tce_mock_node: impl Future<Output = Context>,
+    context_running_tce_test_node: impl Future<Output = Context>,
 ) -> Result<(), Box<dyn std::error::Error>> {
-    let context = context_running_tce_mock_node.await;
+    let context = context_running_tce_test_node.await;
 
     let source_subnet_id: SubnetId = [1u8; 32].into();
-    let prev_certificate_id: CertificateId = [01u8; 32].into();
+    let default_cert_id: CertificateId = [0u8; 32].into();
     let certificate_id: CertificateId = [02u8; 32].into();
 
     info!("Creating TCE node client");
@@ -207,29 +237,38 @@ async fn test_tce_get_source_head_certificate(
         .map(|r| r.into_inner())
         .expect("valid response");
 
-    let expected_dummy_certificate = Certificate {
-        id: Default::default(),
-        prev_id: Default::default(),
+    let expected_default_genesis_certificate = Certificate {
+        id: Some(default_cert_id.clone()),
+        prev_id: Some(default_cert_id.clone()),
         source_subnet_id: Some(source_subnet_id.clone()),
         target_subnets: vec![],
-        ..Default::default()
+        state_root: [0u8; 32].to_vec(),
+        tx_root_hash: [0u8; 32].to_vec(),
+        verifier: 0,
+        proof: Some(StarkProof { value: Vec::new() }),
+        signature: Some(Default::default()),
     };
     let expected_response = GetSourceHeadResponse {
-        certificate: Some(expected_dummy_certificate.clone()),
+        certificate: Some(expected_default_genesis_certificate.clone()),
         position: Some(SourceStreamPosition {
             subnet_id: Some(source_subnet_id.clone()),
-            certificate_id: expected_dummy_certificate.id,
+            certificate_id: expected_default_genesis_certificate.id.clone(),
             position: 0,
         }),
     };
+
     assert_eq!(response, expected_response);
 
     let test_certificate = Certificate {
         source_subnet_id: Some(source_subnet_id.clone()),
         id: Some(certificate_id),
-        prev_id: Some(prev_certificate_id),
+        prev_id: Some(default_cert_id),
         target_subnets: vec![],
-        ..Default::default()
+        state_root: [0u8; 32].to_vec(),
+        tx_root_hash: [0u8; 32].to_vec(),
+        verifier: 0,
+        proof: Some(StarkProof { value: Vec::new() }),
+        signature: Some(Default::default()),
     };
     match client
         .submit_certificate(SubmitCertificateRequest {
@@ -255,12 +294,16 @@ async fn test_tce_get_source_head_certificate(
         .await
         .map(|r| r.into_inner())
         .unwrap();
+
+    // TODO currently only delivered certificates are counted as
+    // head source certificate, so default certificate is expected
+    // Should be updated to count also pending certificates
     let expected_response = GetSourceHeadResponse {
-        certificate: Some(test_certificate.clone()),
+        certificate: Some(expected_default_genesis_certificate.clone()),
         position: Some(SourceStreamPosition {
             subnet_id: Some(source_subnet_id.clone()),
-            certificate_id: test_certificate.id,
-            position: 1,
+            certificate_id: expected_default_genesis_certificate.id,
+            position: 0,
         }),
     };
     assert_eq!(response, expected_response);
