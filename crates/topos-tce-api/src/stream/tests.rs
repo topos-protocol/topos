bd903565a6c516529c12b76a8695121ec0198d59
Freyskeyd <simon.paitrault@gmail.com>
2023-02-15 11:17:00 +0100

    refactor: update cargo.lock and stream tests
    Signed-off-by: Freyskeyd <simon.paitrault@gmail.com>

--- crates/topos-tce-api/src/stream/tests.rs
+++ crates/topos-tce-api/src/stream/tests.rs
@@ -1,9 +1,11 @@
 use crate::runtime::InternalRuntimeCommand;
+use crate::stream::{StreamError, StreamErrorKind};
 use crate::tests::encode;
 use crate::wait_for_command;
 use test_log::test;
 use tokio::spawn;
 use topos_core::api::shared::v1::checkpoints::TargetCheckpoint;
+use topos_core::api::shared::v1::positions::TargetStreamPosition;
 use topos_core::api::shared::v1::SubnetId;
 use topos_core::api::tce::v1::watch_certificates_request::OpenStream;
 use topos_core::api::tce::v1::WatchCertificatesRequest;
@@ -16,18 +18,19 @@ mod utils;
 pub async fn sending_no_message() -> Result<(), Box<dyn std::error::Error>> {
     let (_, stream, mut context) = StreamBuilder::default().build();
 
-    spawn(stream.run());
+    let join = spawn(stream.run());
 
     wait_for_command!(
         context.stream_receiver,
-        matches: Err(status) if status.message() == "No openstream provided"
+        matches: Err(status) if status.message() == "No OpenStream provided"
     );
 
-    let expected_stream_id = context.stream_id;
-
-    wait_for_command!(
-        context.runtime_receiver,
-        matches: InternalRuntimeCommand::StreamTimeout { stream_id } if stream_id == expected_stream_id
+    assert_eq!(
+        Err(StreamError {
+            stream_id: context.stream_id,
+            kind: StreamErrorKind::PreStartError
+        }),
+        join.await?
     );
 
     Ok(())
@@ -43,31 +46,60 @@ pub async fn sending_open_stream_message() -> Result<(), Box<dyn std::error::Err
         target_checkpoint: Some(TargetCheckpoint {
             // FIX: subnet_id isn't following the right subnet format
             target_subnet_ids: vec![SubnetId {
-                value: "stream_a".into(),
+                value: [1u8; 32].into(),
             }],
             positions: Vec::new(),
         }),
         source_checkpoint: None,
     }
     .into();
-    {
-        _ = tx.send_data(encode(&msg)?).await;
-        let _ = tx;
-    }
+
+    _ = tx.send_data(encode(&msg)?).await;
 
     let expected_stream_id = context.stream_id;
 
     wait_for_command!(
         context.runtime_receiver,
-        matches: InternalRuntimeCommand::Register { stream_id, ref subnet_ids, .. } if stream_id == expected_stream_id && subnet_ids == &vec![SubnetId {value: "stream_a".into()}]
+        matches: InternalRuntimeCommand::Register { stream_id, ref subnet_ids, .. } if stream_id == expected_stream_id && subnet_ids == &vec![SubnetId {value: [1u8; 32].into()}]
     );
 
     Ok(())
 }
 
 #[test(tokio::test)]
-#[ignore = "not yet implemented"]
-async fn subscribing_to_multiple_subnet_id() {}
+async fn subscribing_to_one_target_with_position() -> Result<(), Box<dyn std::error::Error>> {
+    let (mut tx, stream, mut context) = StreamBuilder::default().build();
+
+    spawn(async move { stream.run().await });
+
+    let msg: WatchCertificatesRequest = OpenStream {
+        target_checkpoint: Some(TargetCheckpoint {
+            // FIX: subnet_id isn't following the right subnet format
+            target_subnet_ids: vec![SubnetId {
+                value: [1u8; 32].into(),
+            }],
+            positions: vec![TargetStreamPosition {
+                source_subnet_id: Some([2u8; 32].into()),
+                target_subnet_id: Some([1u8; 32].into()),
+                position: 1,
+                certificate_id: None,
+            }],
+        }),
+        source_checkpoint: None,
+    }
+    .into();
+
+    _ = tx.send_data(encode(&msg)?).await;
+
+    let expected_stream_id = context.stream_id;
+
+    wait_for_command!(
+        context.runtime_receiver,
+        matches: InternalRuntimeCommand::Register { stream_id, ref subnet_ids, .. } if stream_id == expected_stream_id && subnet_ids == &vec![SubnetId {value: [1u8; 32].into()}]
+    );
+
+    Ok(())
+}
 
 #[test(tokio::test)]
 #[ignore = "not yet implemented"]
