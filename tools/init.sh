df4101e5fab78e3d22fcb05737458bfdd213e6c0
Thiago Nobayashi <thiago.nobayashi@toposware.com>
2023-03-06 14:18:52 -0300

    chore: adapt init.sh for k8s

--- tools/init.sh
+++ tools/init.sh
@@ -10,6 +10,8 @@ fi
 
 JQ=$(which jq)
 TOPOS_BIN=./topos
+TMP_PEER_LIST_PATH=/tmp/shared/peer_ids.tmp
+TMP_NODE_LIST_PATH=/tmp/shared/peer_nodes.tmp
 BOOT_PEERS_PATH=/tmp/shared/boot_peers.json
 PEER_LIST_PATH=/tmp/shared/peer_ids.json
 NODE_LIST_PATH=/tmp/shared/peer_nodes.json
@@ -21,6 +23,8 @@ case "$1" in
     "boot")
         BOOT_PEER_ID=$($TOPOS_BIN tce keys --from-seed=$TCE_LOCAL_KS)
 
+        # clean up shared directory
+        rm $BOOT_PEERS_PATH $PEER_LIST_PATH $NODE_LIST_PATH 2> /dev/null || echo "everything's clean!"
 
         echo "Generating boot_peers file..."
         $JQ -n --arg PEER $BOOT_PEER_ID --arg ADDR $TCE_EXT_HOST '[$PEER + " " + $ADDR + "/tcp/9090"]' > $BOOT_PEERS_PATH
@@ -59,7 +63,11 @@ case "$1" in
            done
 
            PEER=$($TOPOS_BIN tce keys --from-seed=$HOSTNAME)
-           cat <<< $($JQ --arg PEER $PEER '. += [$PEER]' $PEER_LIST_PATH) > $PEER_LIST_PATH
+
+           if ! grep -q $PEER $PEER_LIST_PATH; then
+               $JQ --arg PEER $PEER '. += [$PEER]' $PEER_LIST_PATH > "${TMP_PEER_LIST_PATH}" \
+                && mv "${TMP_PEER_LIST_PATH}" $PEER_LIST_PATH
+           fi
 
            export TCE_LOCAL_KS=$HOSTNAME
            export TCE_EXT_HOST
@@ -70,7 +78,10 @@ case "$1" in
                sleep 1
            done
 
-           cat <<< $($JQ --arg NODE $NODE '.nodes += [$NODE]' $NODE_LIST_PATH) > $NODE_LIST_PATH
+           if ! grep -q $NODE $NODE_LIST_PATH; then
+               $JQ --arg NODE $NODE '.nodes += [$NODE]' $NODE_LIST_PATH > "${TMP_NODE_LIST_PATH}" \
+                && mv "${TMP_NODE_LIST_PATH}" $NODE_LIST_PATH
+           fi
        fi
 
        exec "$TOPOS_BIN" "$@"
