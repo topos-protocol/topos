af2ada9332dbe3e3c45e3a4bfee96766101ed445
Bastian Gruber <foreach@me.com>
2023-08-28 17:23:49 +0200

    feat: extract validators from extra data field (#284)

--- crates/topos/src/config/genesis/tests.rs
+++ crates/topos/src/config/genesis/tests.rs
@@ -1,6 +1,3 @@
-use std::path::PathBuf;
-
-use libp2p::PeerId;
 use rstest::fixture;
 use rstest::rstest;
 
@@ -28,4 +25,15 @@ pub fn test_parse_bootnodes(genesis: &Genesis) {
     let bootnodes = genesis.boot_peers();
 
     assert_eq!(4, bootnodes.len());
+}
+
+#[rstest]
+pub fn test_extract_validators(genesis: &Genesis) {
+    let validators = genesis.validators();
+
+    assert_eq!(validators.len(), 4);
+    assert_eq!(validators[0], "0x100d617e4392c02b31bdce650b26b6c0c3e04f95");
+    assert_eq!(validators[1], "0x92183cff18a1328e7d791d607589a15d9eee4bc4");
+    assert_eq!(validators[2], "0xb4973cdb10894d1d1547673bd758589034c2bba5");
+    assert_eq!(validators[3], "0xc16d83893cb61872206d4e271b813015d3242d94");
 }
