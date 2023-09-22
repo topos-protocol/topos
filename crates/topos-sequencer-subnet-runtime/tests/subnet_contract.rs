0c480746d37f250648eebfe937fc6b7fc8d054b1
Bastian Gruber <foreach@me.com>
2023-09-22 16:04:54 +0200

    fix: cleanup

--- crates/topos-sequencer-subnet-runtime/tests/subnet_contract.rs
+++ crates/topos-sequencer-subnet-runtime/tests/subnet_contract.rs
@@ -3,8 +3,6 @@
 use dockertest::{
     Composition, DockerTest, Image, LogAction, LogOptions, LogPolicy, LogSource, PullPolicy, Source,
 };
-use ethers::prelude::Block;
-use ethers::types::H256;
 use ethers::{
     abi::{ethabi::ethereum_types::U256, Address},
     contract::abigen,
