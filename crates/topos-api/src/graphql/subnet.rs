05c0255d27ce1af7a8da3836b6cc81d3cbfdf049
Simon Paitrault <simon.paitrault@gmail.com>
2023-10-17 11:25:29 +0200

    feat: adding subnet graphql test
    Signed-off-by: Simon Paitrault <simon.paitrault@gmail.com>

--- crates/topos-api/src/graphql/subnet.rs
+++ crates/topos-api/src/graphql/subnet.rs
@@ -1,6 +1,7 @@
 use async_graphql::{InputObject, SimpleObject};
 use serde::{Deserialize, Serialize};
 use std::str::FromStr;
+use tracing::{error, warn};
 
 use super::errors::GraphQLServerError;
 
@@ -17,7 +18,7 @@ impl TryFrom<&SubnetId> for topos_uci::SubnetId {
 
     fn try_from(value: &SubnetId) -> Result<Self, Self::Error> {
         Self::from_str(value.value.as_str()).map_err(|e| {
-            tracing::error!("Failed to convert SubnetId from GraphQL input {e:?}");
+            error!("Failed to convert SubnetId from GraphQL input {e:?}");
             GraphQLServerError::ParseDataConnector
         })
     }
@@ -25,6 +26,11 @@ impl TryFrom<&SubnetId> for topos_uci::SubnetId {
 
 impl PartialEq<topos_uci::SubnetId> for SubnetId {
     fn eq(&self, other: &topos_uci::SubnetId) -> bool {
-        other.as_array().eq(&self.value.as_bytes())
+        if let Ok(current) = topos_uci::SubnetId::from_str(&self.value) {
+            other.as_array().eq(current.as_array())
+        } else {
+            warn!("Unexpected parsing error for subnet id during comparaison");
+            false
+        }
     }
 }
