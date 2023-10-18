16cb5764e0e0f0fcb578e4f29bc5a53459384408
Simon Paitrault <simon.paitrault@gmail.com>
2023-10-18 15:26:02 +0200

    chore: adding readme check on github
    Signed-off-by: Simon Paitrault <simon.paitrault@gmail.com>

--- crates/topos-tce-broadcast/src/lib.rs
+++ crates/topos-tce-broadcast/src/lib.rs
@@ -1,26 +1,24 @@
 //! Implementation of Topos Reliable Broadcast to be used in the Transmission Control Engine (TCE)
 //!
-//! The implementation is based on the paper: [Topos: A Secure, Trustless, and Decentralized Interoperability Protocol](https://arxiv.org/pdf/2206.03481.pdf)
-//!
 //! This crate is designed to be used as a library in the TCE implementation.
 //! It covers the Reliable Broadcast part of the TCE, which is the core of the TCE.
 //! It doesn't handle how messages are sent or received, nor how the certificates are stored.
 //! It is designed to be used with any transport and storage implementation, relying on the
 //! `ProtocolEvents` and `DoubleEchoCommand` to communicate with the transport and storage.
 //!
-//! The Broadcast allows a set of validators to agree on a set of messages in order to
-//! reach consensus about the delivery of a certificate.
+//! The reliable broadcast allows a set of validators to agree on a set of messages in order to
+//! reach agreement about the delivery of a certificate.
 //!
-//! Each certificates need to be broadcasted to the network, and each validator needs to
-//! receive a set of messages from the other validators in order to reach a threshold.
-//! The threshold is defined by the `ReliableBroadcastParams` and is the minimum number of
-//! validators that need to agree on a certificate in order to consider it delivered.
+//! Each certificates need to be broadcast to the network, and each validator needs to
+//! receive a threshold of messages from the other validators.
+//! The thresholds are defined by the `ReliableBroadcastParams` and correspond to the minimum number of
+//! validators who need to agree on one certificate in order to consider it delivered.
 //!
 //! This crate is responsible for validating and driving the broadcast of every certificates.
 //!
 //! ## Input
 //!
-//! The input of the broadcast is a certificate to be broadcasted. It can be received from
+//! The input of the broadcast is a certificate to be broadcast. It can be received from
 //! the transport layer, or from the storage layer (from the pending tables).
 //!
 //! The transport layer can be anything from p2p network to API calls.
@@ -32,7 +30,8 @@
 //!
 //! The outcome of the broadcast is either a certificate delivered or a failure on the delivery.
 //!
-
+//! The implementation is based on the paper: [Topos: A Secure, Trustless, and Decentralized Interoperability Protocol](https://arxiv.org/pdf/2206.03481.pdf)
+//!
 use double_echo::DoubleEcho;
 use futures::Stream;
 use std::collections::HashSet;
@@ -71,7 +70,7 @@ use crate::sampler::SubscriptionsView;
 
 #[derive(Debug)]
 pub enum TaskStatus {
-    /// The task finished successfully and broadcasted the certificate + received ready
+    /// The task finished successfully and broadcast the certificate + received ready
     Success,
     /// The task did not finish successfully and stopped.
     Failure,
