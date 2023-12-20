5e9def2a9af268b3cbcc55b138dac436a67d2e91
Simon Paitrault <simon.paitrault@gmail.com>
2023-12-20 17:47:35 +0100

    chore: adding test for sigterm
    Signed-off-by: Simon Paitrault <simon.paitrault@gmail.com>

--- crates/topos/tests/config.rs
+++ crates/topos/tests/config.rs
@@ -5,58 +5,9 @@ use std::process::Command;
 use tempfile::tempdir;
 use topos::install_polygon_edge;
 
-async fn setup_polygon_edge(path: &str) -> String {
-    let installation_path = std::env::current_dir().unwrap().join(path);
-    let binary_path = installation_path.join("polygon-edge");
+use crate::utils::setup_polygon_edge;
 
-    if !binary_path.exists() {
-        std::fs::create_dir_all(installation_path.clone())
-            .expect("Cannot create test binary folder");
-
-        install_polygon_edge(
-            "topos-protocol/polygon-edge".to_string(),
-            None,
-            installation_path.clone().as_path(),
-        )
-        .await
-        .expect("Cannot install Polygon Edge binary");
-    }
-
-    installation_path.to_str().unwrap().to_string()
-}
-
-async fn generate_polygon_edge_genesis_file(
-    polygon_edge_bin: &str,
-    home_path: &str,
-    node_name: &str,
-    subnet: &str,
-) -> Result<(), Box<dyn std::error::Error>> {
-    let genesis_folder_path: PathBuf = PathBuf::from(format!("{}/subnet/{}", home_path, subnet));
-    if !genesis_folder_path.exists() {
-        std::fs::create_dir_all(genesis_folder_path.clone())
-            .expect("Cannot create subnet genesis file folder");
-    }
-    let genesis_file_path = format!("{}/genesis.json", genesis_folder_path.display());
-    println!("Polygon edge path: {}", polygon_edge_bin);
-    let mut cmd = Command::new(polygon_edge_bin);
-    let val_prefix_path = format!("{}/node/{}/", home_path, node_name);
-    cmd.arg("genesis")
-        .arg("--dir")
-        .arg(&genesis_file_path)
-        .arg("--consensus")
-        .arg("ibft")
-        .arg("--ibft-validators-prefix-path")
-        .arg(val_prefix_path)
-        .arg("--bootnode") /* set dummy bootnode, we will not run edge to produce blocks */
-        .arg("/ip4/127.0.0.1/tcp/8545/p2p/16Uiu2HAmNYneHCbJ1Ntz1ojvTdiNGCMGWNT5MGMH28AzKNV66Paa");
-    cmd.assert()
-        .success()
-        .stdout(predicate::str::contains(format!(
-            "Genesis written to {}",
-            genesis_folder_path.display()
-        )));
-    Ok(())
-}
+mod utils;
 
 #[tokio::test]
 async fn handle_command_init() -> Result<(), Box<dyn std::error::Error>> {
@@ -320,7 +271,7 @@ async fn command_node_up() -> Result<(), Box<dyn std::error::Error>> {
 
     // Generate polygon edge genesis file
     let polygon_edge_bin = format!("{}/polygon-edge", node_edge_path_env);
-    generate_polygon_edge_genesis_file(
+    utils::generate_polygon_edge_genesis_file(
         &polygon_edge_bin,
         node_up_home_env,
         node_up_name_env,
@@ -340,13 +291,14 @@ async fn command_node_up() -> Result<(), Box<dyn std::error::Error>> {
         .env("TOPOS_NODE_NAME", node_up_name_env)
         .arg("up");
     let mut cmd = tokio::process::Command::from(cmd).spawn().unwrap();
-    let output = tokio::time::timeout(std::time::Duration::from_secs(60), cmd.wait()).await;
+    let output = tokio::time::timeout(std::time::Duration::from_secs(10), cmd.wait()).await;
 
     // Check if node up was successful
     match output {
         Ok(Ok(exit_status)) => {
             if !exit_status.success() {
                 println!("Exited with error output {:?}", exit_status.code());
+                cmd.kill().await?;
                 panic!("Node up failed");
             }
         }
--- crates/topos/tests/utils.rs
+++ crates/topos/tests/utils.rs
@@ -1,4 +1,10 @@
+use assert_cmd::prelude::*;
+use predicates::prelude::*;
 use regex::Regex;
+use std::path::PathBuf;
+use std::process::Command;
+use tempfile::tempdir;
+use topos::install_polygon_edge;
 
 #[cfg(test)]
 pub fn sanitize_config_folder_path(cmd_out: &str) -> String {
@@ -9,4 +15,57 @@ pub fn sanitize_config_folder_path(cmd_out: &str) -> String {
     pattern
         .replace(cmd_out, "[default: /home/runner/.config/topos]")
         .to_string()
+}
+
+pub async fn setup_polygon_edge(path: &str) -> String {
+    let installation_path = std::env::current_dir().unwrap().join(path);
+    let binary_path = installation_path.join("polygon-edge");
+
+    if !binary_path.exists() {
+        std::fs::create_dir_all(installation_path.clone())
+            .expect("Cannot create test binary folder");
+
+        install_polygon_edge(
+            "topos-protocol/polygon-edge".to_string(),
+            None,
+            installation_path.clone().as_path(),
+        )
+        .await
+        .expect("Cannot install Polygon Edge binary");
+    }
+
+    installation_path.to_str().unwrap().to_string()
+}
+
+pub async fn generate_polygon_edge_genesis_file(
+    polygon_edge_bin: &str,
+    home_path: &str,
+    node_name: &str,
+    subnet: &str,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let genesis_folder_path: PathBuf = PathBuf::from(format!("{}/subnet/{}", home_path, subnet));
+    if !genesis_folder_path.exists() {
+        std::fs::create_dir_all(genesis_folder_path.clone())
+            .expect("Cannot create subnet genesis file folder");
+    }
+    let genesis_file_path = format!("{}/genesis.json", genesis_folder_path.display());
+    println!("Polygon edge path: {}", polygon_edge_bin);
+    let mut cmd = Command::new(polygon_edge_bin);
+    let val_prefix_path = format!("{}/node/{}/", home_path, node_name);
+    cmd.arg("genesis")
+        .arg("--dir")
+        .arg(&genesis_file_path)
+        .arg("--consensus")
+        .arg("ibft")
+        .arg("--ibft-validators-prefix-path")
+        .arg(val_prefix_path)
+        .arg("--bootnode") /* set dummy bootnode, we will not run edge to produce blocks */
+        .arg("/ip4/127.0.0.1/tcp/8545/p2p/16Uiu2HAmNYneHCbJ1Ntz1ojvTdiNGCMGWNT5MGMH28AzKNV66Paa");
+    cmd.assert()
+        .success()
+        .stdout(predicate::str::contains(format!(
+            "Genesis written to {}",
+            genesis_folder_path.display()
+        )));
+    Ok(())
 }
