b13dc5831edc35b7253dbe0f6f1efb589d2830cd
Bastian Gruber <foreach@me.com>
2024-01-04 10:56:37 -0400

    feat: remove register command macro

--- crates/topos-tce-gatekeeper/src/builder.rs
+++ crates/topos-tce-gatekeeper/src/builder.rs
@@ -2,7 +2,6 @@ use std::future::IntoFuture;
 
 use futures::{future::BoxFuture, FutureExt};
 use tokio::sync::mpsc;
-use topos_p2p::PeerId;
 
 use crate::{client::GatekeeperClient, Gatekeeper, GatekeeperError};
 
@@ -16,16 +15,11 @@ impl IntoFuture for GatekeeperBuilder {
 
     fn into_future(self) -> Self::IntoFuture {
         let (shutdown_channel, shutdown) = mpsc::channel(1);
-        let (commands, commands_recv) = mpsc::channel(100);
 
         futures::future::ok((
-            GatekeeperClient {
-                shutdown_channel,
-                commands,
-            },
+            GatekeeperClient { shutdown_channel },
             Gatekeeper {
                 shutdown,
-                commands: commands_recv,
                 ..Gatekeeper::default()
             },
         ))
