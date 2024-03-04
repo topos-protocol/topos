83bb270fef4249811e1b2dde04a08d2642ecd490
Simon Paitrault <simon.paitrault@gmail.com>
2024-03-04 09:45:40 +0100

    chore: removing cache_size
    Signed-off-by: Simon Paitrault <simon.paitrault@gmail.com>

--- crates/topos-p2p/src/behaviour/peer_info.rs
+++ crates/topos-p2p/src/behaviour/peer_info.rs
@@ -12,8 +12,7 @@ pub struct PeerInfoBehaviour {
 impl PeerInfoBehaviour {
     pub(crate) fn new(identify_protocol: &'static str, peer_key: &Keypair) -> PeerInfoBehaviour {
         let ident_config = IdentifyConfig::new(identify_protocol.to_string(), peer_key.public())
-            .with_push_listen_addr_updates(true)
-            .with_cache_size(0);
+            .with_push_listen_addr_updates(true);
 
         let identify = Identify::new(ident_config);
 
