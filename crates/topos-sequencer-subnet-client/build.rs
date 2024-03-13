7052ca31ca624f34f1f585cc5496966cdafdd05b
Jawad Tariq <sjcool420@hotmail.co.uk>
2024-03-13 18:00:49 -0400

    feat: add build script for topos-sequencer-subnet-client
    Signed-off-by: Jawad Tariq <sjcool420@hotmail.co.uk>

---
+++ crates/topos-sequencer-subnet-client/build.rs
@@ -0,0 +1,36 @@
+use std::process::{exit, Command};
+
+const CONTRACTS_PATH: &str = "../../contracts";
+
+fn main() {
+    if !CONTRACTS_PATH.is_empty() {
+        std::env::set_current_dir(CONTRACTS_PATH).unwrap_or_else(|err| {
+            eprintln!("Error changing to subdirectory: {}", err);
+            exit(1);
+        });
+    }
+
+    Command::new("npm")
+        .arg("ci")
+        .status()
+        .unwrap_or_else(|err| {
+            eprintln!("Error executing npm ci: {}", err);
+            exit(1);
+        });
+
+    Command::new("npm")
+        .arg("run")
+        .arg("build")
+        .status()
+        .unwrap_or_else(|err| {
+            eprintln!("Error executing npm run build: {}", err);
+            exit(1);
+        });
+
+    std::env::set_current_dir("..").unwrap_or_else(|err| {
+        eprintln!("Error changing back to the original directory: {}", err);
+        exit(1);
+    });
+
+    println!("cargo:rerun-if-changed={}", CONTRACTS_PATH);
+}
