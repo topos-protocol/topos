1eb445b78bb68f0e12ed2152c291978da74ed563
Bastian Gruber <foreach@me.com>
2024-03-27 16:11:44 -0300

    fix: add logging to the spammer command

--- crates/topos-certificate-spammer/src/lib.rs
+++ crates/topos-certificate-spammer/src/lib.rs
@@ -230,7 +230,8 @@ pub async fn run(
     let mut source_subnets = generate_source_subnets(args.local_key_seed, args.nb_subnets)?;
     info!("Generated source subnets: {source_subnets:#?}");
 
-    info!("Target hosts: {:?}", args.target_subnets);
+    info!("Target hosts: {:?}", args.target_hosts);
+    info!("Target subnets: {:?}", args.target_subnets);
 
     // Target subnets (randomly assigned to every generated certificate)
     let target_subnet_ids: Vec<SubnetId> = args
